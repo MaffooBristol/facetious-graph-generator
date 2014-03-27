497a2682e2c85f3d87301c33fd1242ca69790000
Matt Fletcher <naiveamoeba@gmail.com>
2014-03-27 14:15:20 +0000

    Moved coords factory into its own folder/file.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,39 +59,4 @@ angular.module('fggApp', [
         $location.path('/login');
       }
     });
-  })
-  .factory('Coords', function() {
-    var coords = {};
-    coords.bankrupt = {};
-    coords.bankrupt._meta = {
-      title: 'Bankrupt'
-    , category: 'lines'
-    , weight: 0
-    }
-    coords.bankrupt._points = [
-      [20, 20]
-    , [80, 40]
-    , [140, 100]
-    , [200, 110]
-    , [260, 140]
-    , [320, 160]
-    , [380, 290]
-    ];
-
-    coords.success = {};
-    coords.success._meta = {
-      title: 'Success'
-    , category: 'lines'
-    , weight: 1
-    }
-    coords.success._points = [
-      [20, 280]
-    , [80, 260]
-    , [140, 200]
-    , [200, 190]
-    , [260, 160]
-    , [320, 140]
-    , [380, 10]
-    ];
-    return coords;
   });
